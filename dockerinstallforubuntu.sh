66f75b9a5afcd2d8f1810ab9a792789f1005bdbb
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2019-04-09 11:48:44 +0530

    docker installs cript added for ubuntu os

---
+++ dockerinstallforubuntu.sh
@@ -0,0 +1,17 @@
+# to remove old version
+sudo apt-get remove docker docker-engine docker.io
+#the below reamining commandds are docker install commands 
+sudo apt-get update
+ sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y	
+systemctl enable docker.service
